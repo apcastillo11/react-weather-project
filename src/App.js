1c7fd1fdaa0a90b3a15eeb17008d46d098f6ca76
a2dael2dawhy <55224339+apcastillo11@users.noreply.github.com>
2023-12-14 17:39:08 -0800

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://grand-licorice-3046f2.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Alejandra Castillo
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/apcastillo11/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.
+      </footer>
+    </div>
+  );
 }
