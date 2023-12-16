49d7b7d61ff806df8a356eaec8b5440cbe570059
a2dael2dawhy <55224339+apcastillo11@users.noreply.github.com>
2023-12-16 13:13:40 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://grand-licorice-3046f2.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Alejandra Castillo
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/apcastillo11/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub.
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://grand-licorice-3046f2.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alejandra Castillo
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/apcastillo11/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub.
+        </footer>
+      </div>
     </div>
   );
 }
